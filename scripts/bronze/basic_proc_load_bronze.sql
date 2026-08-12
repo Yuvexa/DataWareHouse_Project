42130082f6be4df890f4d984e6af781eff3d8829
Yuvexa <yuvrajpu2003@gmail.com>
2026-08-12 12:36:46 +0530

    Update basic_proc_load_bronze.sql

--- scripts/bronze/basic_proc_load_bronze.sql
+++ scripts/bronze/basic_proc_load_bronze.sql
@@ -51,8 +51,8 @@ BEGIN
 			TABLOCK
 	);
 
-	TRUNCATE TABLE [bronze].[erp_loc_a101];
-	BULK INSERT [bronze].[erp_loc_a101] 
+	TRUNCATE TABLE [bronze].[erp_px_cat_g1v2];
+	BULK INSERT [bronze].[erp_px_cat_g1v2] 
 	FROM 'C:\Users\Admin\Desktop\SQL WITH BARRA\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 	WITH (
 			FIRSTROW = 2,
